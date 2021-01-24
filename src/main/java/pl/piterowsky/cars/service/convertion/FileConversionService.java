1b18df5594cfc868957f54feda015f2da7e21a4f
piterowsky <taatarski@gmail.com>
2021-01-24 12:21:54 +0100

    Remove hardcoded specific type in generic interface

--- src/main/java/pl/piterowsky/cars/service/convertion/FileConversionService.java
+++ src/main/java/pl/piterowsky/cars/service/convertion/FileConversionService.java
@@ -1,7 +1,5 @@
 package pl.piterowsky.cars.service.convertion;
 
-import pl.piterowsky.cars.model.Car;
-
 import java.util.List;
 import java.util.Map;
 
@@ -9,6 +7,6 @@ public interface FileConversionService<T> {
 
     List<T> getKeyValuesAsModel(List<Map<String, String>> listKeyValuePairs);
 
-    Car convertToModel(Map<String, String> keyValuePair);
+    T convertToModel(Map<String, String> keyValuePair);
 
 }
